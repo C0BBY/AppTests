71a18826c79df3b024263c35a7e8802c269e9d8f
C0BBY <aycewhispero@gmail.com>
2018-06-02 20:58:12 +0300

    testing terminal

---
+++ app/src/main/java/com/example/snowden/apptests/first_page/NextClass.java
@@ -0,0 +1,15 @@
+package com.example.snowden.apptests.first_page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.snowden.apptests.R;
+
+public class NextClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_class);
+    }
+}
